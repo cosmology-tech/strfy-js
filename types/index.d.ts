cf9d6122e246b773309763ff749b71dd0a45f084
Dan Lynch <pyramation@gmail.com>
2024-04-13 05:40:12 -0700

    types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,8 @@ interface StringifyOptions {
     replacer?: (this: any, key: string, value: any) => any | null;
     quotes?: 'single' | 'double';
     inlineArrayLimit?: number;
+    camelCase?: boolean;
+    camelCaseFn?: (str: string) => string;
 }
 export declare function chooseQuotes(str: string, preferred: 'single' | 'double' | 'backtick'): string;
 export declare function jsStringify(obj: any, options?: StringifyOptions): string;
