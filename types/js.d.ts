e29be9e9fff39ef50046731f6ad3f7e2a5f04c44
Dan Lynch <pyramation@gmail.com>
2024-05-17 00:08:45 -0700

    types

--- types/js.d.ts
+++ types/js.d.ts
@@ -1,22 +1,44 @@
+export interface JSStringifySetterOptions<ObjType, RootType> {
+    currentKey: string;
+    currentPath: string;
+    value: any;
+    obj: ObjType;
+    root: RootType;
+    defaultValuesMap: {
+        [path: string]: any;
+    };
+}
+export interface JSStringifyPropertyReplacerOptions<ObjType, RootType> {
+    currentKey: string;
+    currentPath: string;
+    propertyRenameMap: {
+        [pattern: string]: string;
+    };
+    obj: ObjType;
+    root: RootType;
+    value?: any;
+}
+export type JSStringifySetter = (options: JSStringifySetterOptions<any, any>) => any;
+export type JSStringifyReplacer = (options: JSStringifyPropertyReplacerOptions<any, any>) => any;
 export interface JSStringifyOptions {
     space?: number;
-    replacer?: (key: string, value: any) => any | null;
-    propertyReplacer?: (currentKey: string, currentPath: string, propertyRenameMap: {
-        [pattern: string]: string;
-    }) => string;
+    propertyReplacer?: (options: JSStringifyPropertyReplacerOptions<any, any>) => string;
+    valueReplacer?: Record<string, (options: JSStringifyPropertyReplacerOptions<any, any>) => any>;
     quotes?: 'single' | 'double' | 'backtick';
     inlineArrayLimit?: number;
     camelCase?: boolean;
     camelCaseFn?: (str: string) => string;
-    propertyRenameMap?: {
-        [key: string]: string;
-    };
+    include?: string[];
+    exclude?: string[];
     defaultValuesMap?: {
         [path: string]: any;
     };
-    defaultValuesSetter?: {
-        [path: string]: (currentKey: string, currentPath: string, value: any, obj?: any) => any;
+    propertyRenameMap?: {
+        [path: string]: any;
     };
+    defaultValuesSetter?: {
+        [path: string]: JSStringifySetter;
+    } | JSStringifySetter;
     json?: boolean;
 }
 export declare function chooseQuotes(str: string, preferred: 'single' | 'double' | 'backtick'): string;
--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,5 +1,13 @@
 export declare function camelCaseTransform(key: string): string;
 export declare function isSimpleKey(key: string): boolean;
+export declare function dirname(path: string): string;
 export declare function escapeStringForSingleQuotes(str: string): string;
 export declare function escapeStringForDoubleQuotes(str: string): string;
 export declare function escapeStringForBacktickQuotes(str: string): string;
+interface ShouldIncludeOptions {
+    include: string[];
+    exclude: string[];
+}
+export declare function expandPaths(paths: string[]): string[];
+export declare const shouldInclude: (type: string, options: ShouldIncludeOptions) => boolean;
+export {};
